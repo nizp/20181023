039b782ec3943229aff5156c3f89000b16efa266
nizp <nzp890524@163.com>
2018-12-14 19:15:45 +0800

    2018-12-14

---
+++ 2018-12-14/helloworld/routes/get.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+/*
+  
+*/
+let person = [
+ 'kaiwen',
+ 'xiaomm'
+];
+
+router.get('/', function(req, res, next) {
+  let obj = {
+    code:0,
+    msg:'有介个银了啦!'
+  }
+  let json = req.query;
+  console.log(person.includes(json.name))
+  if(!person.includes(json.name)){
+      obj.code = 1;
+      obj.msg = '没有介个银!';
+  }
+  res.send(JSON.stringify(obj));
+});
+
+module.exports = router;
